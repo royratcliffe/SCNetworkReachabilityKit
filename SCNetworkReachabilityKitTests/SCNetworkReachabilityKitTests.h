58f1d1752a13f762fcc65223b566a9e3cce6c221
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-08-15 13:47:38 +0100

    System Configuration network reachability kit

---
+++ SCNetworkReachabilityKitTests/SCNetworkReachabilityKitTests.h
@@ -0,0 +1,13 @@
+//
+//  SCNetworkReachabilityKitTests.h
+//  SCNetworkReachabilityKitTests
+//
+//  Created by Roy Ratcliffe on 15/08/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SCNetworkReachabilityKitTests : SenTestCase
+
+@end
---
+++ SCNetworkReachabilityKitTests/SCNetworkReachabilityKitTests.m
@@ -0,0 +1,32 @@
+//
+//  SCNetworkReachabilityKitTests.m
+//  SCNetworkReachabilityKitTests
+//
+//  Created by Roy Ratcliffe on 15/08/2011.
+//  Copyright 2011 Pioneering Software, United Kingdom. All rights reserved.
+//
+
+#import "SCNetworkReachabilityKitTests.h"
+
+@implementation SCNetworkReachabilityKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SCNetworkReachabilityKitTests");
+}
+
+@end
